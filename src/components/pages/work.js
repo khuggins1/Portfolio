d37b1ed6d7820cab7b3c908935c21c11b0322dea
Kassim <kassimhuggins@macbook-pro.attlocal.net>
2021-03-14 23:48:34 -0700

    links in footer

--- src/components/pages/work.js
+++ src/components/pages/work.js
@@ -7,7 +7,7 @@ const Work = () => (
     <section id="mywork">
     <h2>My Work</h2>
       <div id="box">
-      <a class="flexbox-item-1" href="https://github.com/khuggins1/run-buddy.git">Run Buddy</a>
+      <a class="flexbox-item-1" href="https://khuggins1.github.io/run-buddy/">Run Buddy</a>
       <a class="flexbox-item-2" href="https://clhurlbut.github.io/virtual_fortune_cookie/">Fortune Cookie</a>
     </div>  
   </section>
